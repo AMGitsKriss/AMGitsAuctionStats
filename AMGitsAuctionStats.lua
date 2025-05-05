a915b85daaf80f0fb88be5c78b13163abfc5b713
Kriss Jessop <kriss@k-jessop.co.uk>
2025-05-05 02:31:02 +0100

    Min & max prices by hour and date

--- AMGitsAuctionStats.lua
+++ AMGitsAuctionStats.lua
@@ -7,28 +7,46 @@ local function PrintTable(tbl)
     end
 end
 
+local function GetServerName()
+    return GetRealmName().."-"..GetCVar("portal")
+end
+
 local function SaveAuctionData()
     local numAuctions = GetNumAuctionItems("list")
     for i = 1, numAuctions do
-        local name, texture, count, quality, canUse, level, levelColHeader, minBid, minIncrement, buyoutPrice, 
+        local itemName, texture, count, quality, canUse, level, levelColHeader, minBid, minIncrement, buyoutPrice, 
             bidAmount, highBidder, bidderFullName, owner, ownerFullName, saleStatus, itemId, hasAllInfo = GetAuctionItemInfo("list", i)
-        local viewDate = date("%Y-%m-%d-%H")
-
-        --print("Auction [" .. i .."]:", name, texture, count, quality, canUse, level, levelColHeader, minBid, minIncrement, buyoutPrice, 
+        local rawServerTime = C_DateAndTime.GetCurrentCalendarTime()
+        local viewDate = format("%04d-%02d-%02d %02d:00", rawServerTime.year, rawServerTime.month, rawServerTime.monthDay, rawServerTime.hour)
+        print(viewDate)
+        --print("Auction [" .. i .."]:", itemName, texture, count, quality, canUse, level, levelColHeader, minBid, minIncrement, buyoutPrice, 
         --    bidAmount, highBidder, bidderFullName, owner, ownerFullName, saleStatus, itemId, hasAllInfo)
 
-        table.insert(AMGitsAuctionStats[viewDate],
-            {
-                itemName = name,
+        local unitPrice = buyoutPrice / count
+
+        if AMGitsAuctionStats[GetServerName()] == nil then
+            AMGitsAuctionStats[GetServerName()] = {}    
+        end   
+        if AMGitsAuctionStats[GetServerName()][viewDate] == nil then
+            AMGitsAuctionStats[GetServerName()][viewDate] = {}
+        end
+
+        local itemStats = AMGitsAuctionStats[GetServerName()][viewDate][itemId]
+        if itemStats == nil then 
+            itemStats = {
                 itemId = itemId,
-                quantity = count,
-                buyoutTotal = buyoutPrice,
-                buyoutPer = buyoutPrice,
-                bidTotal = minBid,
-                bidPer = minBid,
-                owner = owner
-            }
-        )
+                itemName = itemName,
+                minUnitPrice = unitPrice,
+                maxUnitPrice = unitPrice
+            } 
+        else
+            if unitPrice > 0 and itemStats.minUnitPrice > unitPrice then
+                itemStats.minUnitPrice = unitPrice
+            elseif unitPrice > 0 and itemStats.maxUnitPrice < unitPrice then
+                itemStats.maxUnitPrice = unitPrice
+            end
+        end
+        AMGitsAuctionStats[GetServerName()][viewDate][itemId] = itemStats
 
         -- PrintTable(AMGitsAuctionStats[auctionId .. "|" .. viewDate])
     end
@@ -45,3 +63,12 @@ frame:SetScript("OnEvent", function(_, event)
         SaveAuctionData()
     end
 end)
+
+
+SLASH_AMG1 = "/amg"  -- Defines "/myaddon" as a slash command
+SlashCmdList.AMG = function(msg)
+    if msg == "clear" then
+        AMGitsAuctionStats = {}
+        print("Cleared all data.")
+    end
+end
--- readme.md
+++ readme.md
@@ -1,18 +1,15 @@
-Extracts marketplace data from either page searches, or a whole scan if using a mods like Auctioneer. 
-
-Currently saves a copy of every search result and will include duplicates. Will soon save min and max prices for an item. 
+Extracts marketplace data from either page searches, or a whole scan if using a mods like Auctioneer. Will save min and max prices for an item. 
 
 
 ```
-["2025-05-04-23"] = {
-	{
-		["itemName"] = "Winterfall Firewater",
-		["bidPer"] = 80100,
-		["bidTotal"] = 80100,
-		["quantity"] = 1,
-		["buyoutTotal"] = 84484,
-		["buyoutPer"] = 84484,
-		["itemId"] = 12820,
+	["Thunderstrike-EU"] = {
+		["2025-05-05 03:00"] = {
+			[3576] = {
+				["minUnitPrice"] = 115,
+				["itemName"] = "Tin Bar",
+				["maxUnitPrice"] = 200,
+				["itemId"] = 3576,
+			},
+		},
 	}
-}
 ```
