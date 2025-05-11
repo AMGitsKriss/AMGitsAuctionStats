29927cb0a981aa52043a001e1f3bc2b6cf7d8738
Kriss Jessop <kriss@k-jessop.co.uk>
2025-05-11 15:03:48 +0100

    Process no more than 1000 per frame

--- AMGitsAuctionStats.lua
+++ AMGitsAuctionStats.lua
@@ -1,5 +1,6 @@
 -- Initialize the SavedVariables table
 AMGitsAuctionStats = AMGitsAuctionStats or {}
+ServerName = nil
 
 local function PrintTable(tbl)
     for key, value in pairs(tbl) do
@@ -8,30 +9,58 @@ local function PrintTable(tbl)
 end
 
 local function GetServerName()
-    return GetRealmName().."-"..GetCVar("portal").."-"..UnitFactionGroup("player")
+    return GetRealmName().."-"..GetCVar("portal")
 end
 
-local function SaveAuctionData()
+local function GetAuctionHouseFaction()
+    if GetSubZoneText() == "Booty Bay" then
+        return "Neutral"
+    end
+    return UnitFactionGroup("player")
+end
+
+local function BulkScanAuctionHouse()
+    print("Starting bulk auction.")
+    
+    local query = {
+        searchString = "",
+        sorts = {
+            {sortOrder = Enum.AuctionHouseSortOrder.Price, reverseSort = false},
+            {sortOrder = Enum.AuctionHouseSortOrder.Name, reverseSort = false},
+        }
+    }
+    C_AuctionHouse.SendBrowseQuery(query)
+end
+
+local function SaveAuctionDataBatch(startIndex, endIndex)
     local numAuctions = GetNumAuctionItems("list")
-    for i = 1, numAuctions do
+    local serverName = GetServerName()
+    local ahFaction = GetAuctionHouseFaction()
+
+    local rawServerTime = C_DateAndTime.GetCurrentCalendarTime()
+    local viewDate = format("%04d-%02d-%02d %02d:00", rawServerTime.year, rawServerTime.month, rawServerTime.monthDay, rawServerTime.hour)
+
+    if endIndex > numAuctions then
+        endIndex = numAuctions 
+    end
+
+    for i = startIndex, endIndex do
         local itemName, texture, count, quality, canUse, level, levelColHeader, minBid, minIncrement, buyoutPrice, 
             bidAmount, highBidder, bidderFullName, owner, ownerFullName, saleStatus, itemId, hasAllInfo = GetAuctionItemInfo("list", i)
-        local rawServerTime = C_DateAndTime.GetCurrentCalendarTime()
-        local viewDate = format("%04d-%02d-%02d %02d:00", rawServerTime.year, rawServerTime.month, rawServerTime.monthDay, rawServerTime.hour)
-
-        --print("Auction [" .. i .."]:", itemName, texture, count, quality, canUse, level, levelColHeader, minBid, minIncrement, buyoutPrice, 
-        --    bidAmount, highBidder, bidderFullName, owner, ownerFullName, saleStatus, itemId, hasAllInfo)
 
         local unitPrice = buyoutPrice / count
 
-        if AMGitsAuctionStats[GetServerName()] == nil then
-            AMGitsAuctionStats[GetServerName()] = {}    
+        if AMGitsAuctionStats[serverName] == nil then
+            AMGitsAuctionStats[serverName] = {}    
         end   
-        if AMGitsAuctionStats[GetServerName()][viewDate] == nil then
-            AMGitsAuctionStats[GetServerName()][viewDate] = {}
+        if AMGitsAuctionStats[serverName][ahFaction] == nil then
+            AMGitsAuctionStats[serverName][ahFaction] = {}    
+        end   
+        if AMGitsAuctionStats[serverName][ahFaction][viewDate] == nil then
+            AMGitsAuctionStats[serverName][ahFaction][viewDate] = {}
         end
 
-        local itemStats = AMGitsAuctionStats[GetServerName()][viewDate][itemId]
+        local itemStats = AMGitsAuctionStats[serverName][ahFaction][viewDate][itemId]
         if itemStats == nil then 
             itemStats = {
                 itemId = itemId,
@@ -46,12 +75,16 @@ local function SaveAuctionData()
                 itemStats.maxUnitPrice = unitPrice
             end
         end
-        AMGitsAuctionStats[GetServerName()][viewDate][itemId] = itemStats
-
-        -- PrintTable(AMGitsAuctionStats[auctionId .. "|" .. viewDate])
+        AMGitsAuctionStats[serverName][ahFaction][viewDate][itemId] = itemStats
     end
-end
 
+    print("Pricessed items ".. startIndex .. " through to ".. endIndex .. " auctions")
+
+    if endIndex < numAuctions then
+        C_Timer.After(0, function() SaveAuctionDataBatch(endIndex+1, endIndex+1000) end)
+    end
+    
+end
 
 print("AMGitsAuctionStats Loaded.")
 -- Create a frame to listen for events
@@ -60,7 +93,7 @@ frame:RegisterEvent("AUCTION_ITEM_LIST_UPDATE")
 
 frame:SetScript("OnEvent", function(_, event)
     if event == "AUCTION_ITEM_LIST_UPDATE" then
-        SaveAuctionData()
+        SaveAuctionDataBatch(1, 1000)
     end
 end)
 
@@ -70,5 +103,9 @@ SlashCmdList.AMG = function(msg)
     if msg == "clear" then
         AMGitsAuctionStats = {}
         print("Cleared all data.")
+    elseif msg == "faction" then
+        print(GetAuctionHouseFaction())
+    elseif msg == "scan" then
+        print(BulkScanAuctionHouse())
     end
 end
